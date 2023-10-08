29e3f644f8789d7ef52168918893c2eea9392982
RizkyFirdiansyah <mr.frdsyh@gmail.com>
2023-10-08 20:19:32 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,26 @@
+var config = {
+  texts: [
+    "Hai👋",
+    "Apa Kabar?, Semoga Sehat Ya",
+    "Udah Ultah Lagi aja",
+    "Doa Baik-Baik Untukmu Shifa",
+    "Maaf, Tidak Bisa Ngasih Apa-Apa",
+    "Cuma Kata dan Doa Yang Bisa Ku Kasih",
+    "Dan",
+    "Gimana Tempat Barumu?",
+    "Semoga Betah Ya",
+    "Sekali Lagi, Happy Birthday To You",
+    "🎉🎉🎉🎈🎉🎉🎉",
+  ],
+
+  desc: {
+    turn_on: "Mulai",
+    play: "play",
+    bannar_coming: "Klik",
+    balloons_flying: "Klik Lagi",
+    cake_fadein: "Kue?",
+    light_candle: "Lilin?",
+    wish_message: "Happy Birthday Shifa Aulia Salsabila Prasetyo",
+    story: "Pesan Untukmu",
+  },
+};
